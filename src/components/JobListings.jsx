9423447ead659b52bf15396b2da10f6551fa5c26
AbRahmanMamnoon <abdulrahmanmamnoon82!@gmail.com>
2024-06-14 11:50:33 +0430

    now we fetching data like from backend api, so this job is done via json-server

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,18 +1,46 @@
-import jobs from "../../jobs.json";
+import { useState, useEffect } from "react";
 import JobListing from "./JobListing";
-const JobListings = () => {
-  const recentjobs = jobs.slice(0, 3);
+import Spinner from "./Spinner";
+
+
+const JobListings = ({ isHome = false }) => {
+
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const apiUrl = isHome ? '/api/jobs?_limit=3' : '/api/jobs';
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('Error while fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchJobs();
+  }, []);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "All Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentjobs.map((job) => (
-            <JobListing key={job.id} job={job}/>
-          ))}
-        </div>
+        
+          {loading ?
+            (<Spinner loading={loading} />) :
+            (<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {jobs.map((job) => (
+                <JobListing key={job.id} job={job} />
+              ))}
+           </div>)
+          }
+        
       </div>
     </section>
   )
--- src/components/ViewAllJobs.jsx
+++ src/components/ViewAllJobs.jsx
@@ -3,7 +3,7 @@ const ViewAllJobs = () => {
   return (
     <section className="m-auto max-w-lg my-10 px-6">
       <a
-        href="/jobs"
+        href="/JobsPage"
         className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
       >View All Jobs</a
       >
